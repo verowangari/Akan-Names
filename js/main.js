d7dccb5b6dc05923c4e5c2ed982048aa5212aadb
verowangari <verowangari34@gmail.com>
2021-12-12 18:13:21 +0300

    create  variables with male and female names

--- js/main.js
+++ js/main.js
@@ -1,26 +1,16 @@
 const date = document.getElementById("date")
 const name = document.getElementById("name")
-const callName = document.getElementById("names");
+const callName = document.getElementById("names")
 const form = document.getElementById("form")
+const gender= document.getElementById("gender")
 const errorElement=document.getElementById("error")
 
 callName.addEventListener("click",(e) => {
-    e.preventDefault();
-
-    let myBirthday=input.value;
-
-    prompt.innerHTML = myBirthday;
-
-    console.log(myBirthday);
-    displayName();
-});
-const displayName = () => {
-
-    let maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi","Kwame"];
-
-    let myBirthday=input.value;
-    if (myBirthday.length> maleNames.length)
-}
+    e.preventDefault();}
+    );
+var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi","Kwame" ]
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    
 //end of trial two
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
