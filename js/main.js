c7bfe178df8343cfef7707d884b9201b9707e1a8
verowangari <verowangari34@gmail.com>
2021-12-12 14:36:54 +0300

    validate the form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+const date = document.getElementById("date")
+const name = document.getElementById("name")
+const form = document.getElementById("form")
+const errorElement=document.getElementById("error")
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let messages =[]
+    if (name.value=== '' || name.value==null){
+        messages.push( 'Name is required')
+    }
+    if (messages.length>0){
+        e.preventDefault()
+        errorElement.innerText=messages.join(' , ')
+    }
+
+})
 
