71dfcc44b76718d2b8f9633c1cf7caa56f5f8057
verowangari <verowangari34@gmail.com>
2021-12-13 11:23:27 +0300

    change the order of female and male names to begin with monday

--- js/main.js
+++ js/main.js
@@ -10,13 +10,14 @@ function getDay() {
   let YY = parseInt(year.value);
   YY = YY.toString().substr(1, 4);
   let CC = YY.toString().substr(0, 2);
-  let d = (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+  let d = (((CC / 4) - 2 * CC - 1) + (5 * YY) / 4 + ((26 * (MM + 1) / 10)) + DD) % 7;
   d = Math.floor(d);
   return d;
 }
 
 function getAkan() {
   let daysofWeek = [
+    
     "Monday",
     "Tuesday",
     "Wednesday",
@@ -26,28 +27,25 @@ function getAkan() {
     "Sunday",
   ];
   let maleAkan = [
-    "Kwasi",
     "Kwadwo",
     "Kwabena",
     "Kwaku",
     "Yaw",
     "Kofi",
     "Kwame",
+    "Kwasi",
   ];
-  let femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  let femaleAkan = ["Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua"];
   let index = getDay();
   if (male.checked) {
     let yourAkanName = `Your Akan Name is ${maleAkan[index]} and you were born on ${daysofWeek[index]}`;
     alert(yourAkanName);
-  } else  {
+  } else {
     let yourAkan = `Your Akan Name is ${femaleAkan[index]} and you were born on ${daysofWeek[index]}`;
     alert(yourAkan);
   }
 }
-function validate(){
-
-
-}
+function validate() {}
 btn.addEventListener("click", function (e) {
   e.preventDefault();
   getAkan();
